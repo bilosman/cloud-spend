8bfeae9ff8cb0dddbf2f7d5656702dc8c7687d66
bilosman <abho.osman@gmail.com>
2024-11-09 23:35:33 -0500

    Cloud Spend

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/bilosman/cloud-spend/internal/parser"
+	"github.com/bilosman/cloud-spend/internal/api"
+	"github.com/bilosman/cloud-spend/internal/calculator"
+	"github.com/bilosman/cloud-spend/internal/report"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
